7b316333625ea64cf31fd2205d8c62e659f9ce07
unknown <diliprasanna96@gmail.com>
2023-02-16 10:28:57 +0530

    update

---
+++ 02_Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.dto;
+
+public class CustomerDTO {
+    private String name;
+    private String nic;
+    private String address;
+    private String email;
+    private String image;
+}
