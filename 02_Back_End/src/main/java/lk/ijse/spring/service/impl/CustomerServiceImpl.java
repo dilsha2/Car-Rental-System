7b13e9b4915d3806ca7f50dd6d8ade14f37a9f53
unknown <diliprasanna96@gmail.com>
2023-02-18 20:39:54 +0530

    create some changes in customer service impl

--- 02_Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
+++ 02_Back_End/src/main/java/lk/ijse/spring/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,11 @@
 package lk.ijse.spring.service.impl;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import lk.ijse.spring.repo.CustomerRepo;
 import lk.ijse.spring.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,18 +14,28 @@ import java.util.List;
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public void saveCustomer(CustomerDTO customerDTO) {
+       customerRepo.save(mapper.map(customerDTO, Customer.class));
 
     }
 
     @Override
     public String updateCustomer(CustomerDTO customerDTO) {
+        customerRepo.save(mapper.map(customerDTO, Customer.class));
         return null;
     }
 
     @Override
     public void deleteCustomer(String id) {
+        customerRepo.existsById(id);
 
     }
 
