e5e53469c898413d379b101d0dd5c3e9c5f9edb3
unknown <diliprasanna96@gmail.com>
2023-02-18 18:22:42 +0530

    create ajax in customer register form

---
+++ 01_Front_End/assets/js/customerController.js
@@ -0,0 +1,79 @@
+$('#register-form-date').val(today);
+$('#customer-home-pickup').val(today);
+$('#customer-home-return').val(tomorrow);
+
+var customer;
+var customer_nic;
+
+$('#registerNowBtn').click(function (){
+    // registerFormValidation();
+})
+
+function registerCustomer() {
+    var data = new FormData();
+
+    let nic = $("#register-form-NIC-image")[0].file[0];
+    let nicFileName = nic.name;
+
+
+    data.append("file",nic);
+
+    let CustomerDTO = {
+        nic: $("#register-form-nic").val(),
+        user_name: $("#register-form-user-name"),
+        password: $("#register-form-password").val(),
+        customer_name: $("#register-form-name").val(),
+        address: $("#register-form-address").val(),
+        mobile: $("#register-form-mobile").valueOf(),
+        email: $("#register-form-email").val(),
+        register_date: $("#register-form-date").val(),
+        nic_img: nicFileName,
+    }
+
+    dat.append("customer",new Blob([JSON.stringify(CustomerDTO)]))
+
+    $.ajax({
+        url: baseUrl +"",
+        method: 'post',
+        async: true,
+        contentType: false,
+        processData: false,
+        data: data,
+
+        success: function (resp){
+            alert(resp.message);
+            if (resp.data==null){
+                openCustomerHome(resp.data)
+                getAvailableCar();
+            }
+        },
+        error: function (err) {
+            console.log(err);
+
+        }
+    });
+
+    cleanRegisterForm()
+}
+
+function cleanRegisterForm() {
+    $('#register-form-name,#register-form-nic,#register-form-email,#register-form-mobile, #register-form-address,#register-form-user-name ,#register-form-password, #register-form-NIC-image, #register-form-License-image').css({
+        border: '1px solid gray',
+    })
+    $('#register-form-name,#register-form-nic,#register-form-email,#register-form-mobile, #register-form-address,#register-form-user-name ,#register-form-password, #register-form-NIC-image, #register-form-License-image').val("")
+
+}
+
+function openCustomerHome(){
+    $("#registerForm").css("display", "none")
+
+    $("#customer").css("display", "block")
+    $("#customerNavbar").css("display", "block")
+
+    $("#customer-profile-nic").val(data.nic)
+    $("#customer-profile-name").val(data.user_name)
+    $("#customer-profile-email").val(data.email)
+    $("#customer-profile-address").val(data.address)
+    $("#customer-profile-mobile").val(data.mobile)
+}
+
---
+++ 01_Front_End/assets/js/validation.js
@@ -0,0 +1,5 @@
+var regExCusName = /^[A-z.\s]{3,50}$/;
+var regExCusAddress = /^[A-z\s+]{5,50}$/;
+var regExCusNic = /^[0-9]{12}\b|[0-9]{10}[V]$/;
+var regExCusMobile = /^(07)([125678])(-?)[0-9]{7}$/;
+var regExCusEmail = /^[a-z0-9]{4,20}[@][a-z]{3,6}(.com|.lk)$/;
