0d12c9a26687072a196b1fe0b9d294fc172e7ac6
unknown <diliprasanna96@gmail.com>
2023-02-19 12:24:08 +0530

    create car js

--- 02_Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
+++ 02_Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -10,4 +10,17 @@ import lombok.ToString;
 @Data
 @ToString
 public class CarDTO {
+    private String registration_no;
+    private String brand;
+    private String type;
+    private String transmission;
+    private String color;
+    private int no_of_passengers;
+    private long mileage;
+    private String fuelType;
+    private double daily_rate;
+    private double monthly_rate;
+    private double price_for_extra_km;
+    private double waiver_payment;
+    private String status;
 }
---
+++ 02_Back_End/src/main/java/lk/ijse/spring/dto/CarImageDTO.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CarImageDTO {
+    private String image1;
+    private String image2;
+    private String image3;
+    private String image4;
+}
